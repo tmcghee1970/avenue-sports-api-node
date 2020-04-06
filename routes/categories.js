69a6bbaf431272cae2566851e0027fa83a8b07c0
Tyrone McGhee <tmcghee1970@gmail.com>
2020-04-06 17:51:54 -0400

    Implemented Categories PUT, DELETE, GET id Methods

--- routes/categories.js
+++ routes/categories.js
@@ -19,4 +19,40 @@ router.post("/", async (req, res) => {
     res.send(category);
 });
 
+router.put("/:id", async (req, res) => {
+    const { error } = validate(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
+    
+    const category = await Category.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
+        new: true
+    });
+
+    if (!category)
+        return res.status(404).send("The category with given ID was not found.");
+    
+    res.send(category);
+});
+
+router.delete("/:id", async (req, res) => {
+    const category = await Category.findByIdAndRemove(req.params.id);
+   
+    if (!category)
+        return res.status(404).send("The category with given ID was not found.");
+   
+    res.send(category);
+});
+
+router.get("/:id", async (req, res) => {
+    const category = await Category.findById(req.params.id);
+    
+    if (!category)
+        return res.status(404).send("The category with given ID was not found.");
+
+    res.send(category);
+});
+
+
+
+
+
 module.exports = router;
